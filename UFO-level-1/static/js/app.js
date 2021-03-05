1ed28d82328979d319b9f33d6ca097ebb091040b
floresfdo <fer.flores@outlook.com>
2021-03-04 19:18:06 -0800

    pushing level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,49 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+console.log(tableData);
+
+tableData.forEach(function (sighting){
+    console.log(sighting);
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]){
+        console.log(key,value);
+        var cell = row.append("td");
+        cell.text(value);
+    })
+})
+
+var button = d3.select("button");
+
+var form = d3.select("form");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter (){
+    
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+    var inputdate = inputElement.property("value");
+
+    console.log(inputdate);
+    console.log(tableData);
+
+    var filteredsightings = tableData.filter(tableData => tableData.datetime === inputdate);
+    console.log(filteredsightings);
+    tbody.html("");
+
+    filteredsightings.forEach(function (sighting){
+        console.log(sighting);
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(function([key, value]){
+            console.log(key,value);
+            var cell = row.append("td");
+            cell.text(value);
+        })
+    })
+
+}
